ca86d8596c358d9a16c13b143177311168a91ce1
VarieMoyo <167184766+VarieMoyo@users.noreply.github.com>
2024-04-23 14:48:34 +0200

    make name longer

--- main.R
+++ main.R
@@ -3,10 +3,10 @@ library("forcats")
 library("dplyr")
 library("here")
 
-tv_hours_tab<- gss_cat %>% 
+tv_hours_tabulation<- gss_cat %>% 
   filter(age< 30) %>% 
   group_by(marital) %>% 
   summarize(mean_tvhours = mean(tvhours,na.rm=T))
 
-write.csv(tv_hours_tab,here("TV_Hours_by_Marital.csv"))
+write.csv(tv_hours_tabulation,here("TV_Hours_by_Marital.csv"))
 
